0ab27e97b0b8179b51d366e3b087c1073a6b5650
Holly Parsons <holly-parsons@outlook.com>
2021-07-17 17:43:13 -0500

    Add project files.

---
+++ sensitiveInfo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace sensitive_info
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your information");
+            var value = Console.ReadLine();
+            var removeValue = value.Length - 4;
+            var lastFour = value.Remove(0, removeValue);
+            var remainingValue = value.Remove(removeValue);
+            var hiddenValues = new System.Text.StringBuilder();
+            for (var i = 0; i < remainingValue.Length; i++)
+            {
+                hiddenValues.Append('*');
+            }
+            Console.WriteLine(hiddenValues + lastFour);
+        }
+    }
+}
